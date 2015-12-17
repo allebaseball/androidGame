e5c25300ad534f39703defab263314c91d364420
allebaseball <allebaseball@hotmail.it>
2015-12-17 02:29:26 +0100

    Really added Constant class

---
+++ core/src/com/mygdx/game/Const.java
@@ -0,0 +1,19 @@
+package com.mygdx.game;
+
+public class Const {
+    // screen size
+    public static final int V_WIDTH = 416 * 16 / 9;
+    public static final int V_HEIGHT = 416;
+
+    // tile size
+    public static final int TILE_WIDTH = 32;
+    public static final int TILE_HEIGHT = 32;
+
+    // world variables
+    public static final float GRAVITY = 220*1.8f;
+    public static final float SPEED_X = 50*2;
+    public static final float SPEED_Y = 120*2;
+
+    // strings
+    public static final String MAP_PATH = "maps/TestTiledMapTest.tmx";
+}
